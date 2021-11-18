39e2f8a4134a4805c35595daf6607d5f1bb8e303
LABO13-13-USER <LABO13-13-USER@satis.local>
2021-11-18 21:10:59 +0100

    1

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>La loose</title>
+        <a href="info.php">Cliquez ici pour plus d'infos</a>
+    </head>
+    <body>
+        <h1>Les débuts de la loose</h1>
+            <p>C'est la première fois que je crée un site et je n'en suis pas peu fière. 
+            Néanmoins, ça reste pas si extroardinaire.</p>
+        <h2>La pratique se fait attendre</h2>
+            <p>Malheureusement, la pratique n'avait pas l'air d'être la prioritée de ce cours. 
+            Point n'en faut, me dis-je. Ce sera pour la prochaine fois.</p>
+    </body>
+</html>
